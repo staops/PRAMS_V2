2b068cbf09d78b7cef5ef4d88115f49f023d974e
Alex Gonzalez <edwinglzmtz@outlook.com>
2024-08-18 16:25:24 -0600

    feat: PRAMS-30 - New Agencies methods to manage inactive items

---
+++ PRAMS.Domain/Entities/Agencies/Dto/AgenciaSearchDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PRAMS.Domain.Entities.Agencies.Dto
+{
+    public class AgenciaSearchDto
+    {
+        public required string TipoAgencia { get; set; }
+        public required string NombreAgencia { get; set; }
+        public required string Region { get; set; }
+    }
+}
